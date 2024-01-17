504c3b4b792d96cd9f37da760a6e46c8d765cdb2
Henrizn <henricks032@gmail.com>
2024-01-16 22:00:29 -0300

    new file:   .vscode/settings.json 	new file:   150knightcoins.html 	new file:   Assets/imgs/LOGO DISCORD.png 	renamed:    Assets/imgs/INSTAGRAM white.png -> Assets/imgs/LOGO INSTAGRAM.png 	new file:   Assets/imgs/LOGOMARCA NIGHTCRAFT.png 	new file:   Assets/imgs/background.jpeg 	new file:   Assets/imgs/diamante.png 	new file:   Assets/imgs/esmeralda.png 	deleted:    Assets/imgs/logo-whatsapp-png-transparente12.png 	deleted:    Assets/imgs/logoacai.jpg 	deleted:    Assets/imgs/logoacai.png 	new file:   Assets/imgs/nightcoin.png 	new file:   Assets/imgs/ouro.png 	deleted:    Assets/imgs/point-do-acai.png 	deleted:    Assets/imgs/restaurantes-em-curitiba-03.png 	deleted:    Assets/logoacai.psd 	new file:   README.md 	new file:   icon.ico 	modified:   index.html 	modified:   main.js 	modified:   style.css 	new file:   vip-diamante.html 	new file:   vip-esmeralda.html 	new file:   vip-ouro.html

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 align="center">NightCraft - Skyblock (Site)</h1>
+<p align="center">
+<img loading="lazy" src="http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge">
+</p>
+
+# Resumo do projeto
+Projeto em andamento, sendo realizado pela empresa NightCraft, visando mostrar as informações do seu servidor de Skyblock.
+
+# ✔️ Tecnologias utilizadas
+- ``HTML``
+- ``CSS``
+- ``Javascript``
+
+- # 📁 Acesso ao projeto
+Você pode <a href="">acessar o código-fonte clicando aqui</a>
+# 🛠️ Abrir e rodar o projeto
+Você pode abrir o <a href="">site do projeto clicando aqui</a>
--- main.js
+++ main.js
@@ -1,159 +1,68 @@
-let logado = false
+const catalogo = [
+    {
+        id: 1,
+        nome: 'Vip Esmeralda',
+        duracao: '30 dias',
+        preco: 29.99,
+        desconto: 19.99,
+        imagem: 'esmeralda.png',
+        site: 'vip-esmeralda.html',
+    },
+    {
+        id: 2,
+        nome: 'Vip Ouro',
+        duracao: '90 dias',
+        preco: 49.99,
+        desconto: 39.99,
+        imagem: 'ouro.png',
+        site: 'vip-ouro.html'
+    },
+    {
+        id: 3,
+        nome: 'Vip Diamante',
+        duracao: '365 dias',
+        preco: 99.99,
+        desconto: 79.99,
+        imagem: 'diamante.png',
+        site: 'vip-diamante.html'
+    },
+    {
+        id: 4,
+        nome: '150k NightCoins',
+        duracao: 'Uso único',
+        preco: 11.99,
+        desconto: 7.99,
+        imagem: 'nightcoin.png',
+        site: '150knightcoins.html'
+    }
+]
 
-function openLogin() {
-    if (logado == true) {
-        openConfirmLogin()
-    } else {
-        document.getElementById('login').classList.remove("closedLogin")
-        document.getElementById('login').classList.add("openedLogin")
+function produtos() {
+    for (const produtoCatalogo of catalogo) {
+        const cardProduto =
+            `<div class="produto" id='produto-${produtoCatalogo.id}'>
+                <img 
+                    src="./Assets/imgs/${produtoCatalogo.imagem}"
+                />
+                <p class='nome'>${produtoCatalogo.nome}</p>
+                <p class='duracao'>(${produtoCatalogo.duracao})</p>
+                <p class='preco'><s>R$${produtoCatalogo.preco}</s></p>
+                <p class='desconto'>R$${produtoCatalogo.desconto}</p>
+                <a href="./${produtoCatalogo.site}" class="info-item">Mais Informações</a>
+            </div>`;
+
+        document.getElementById("container-products").innerHTML += cardProduto
     }
 }
 
-function closeLogin() {
-    document.getElementById('login').classList.remove("openedLogin")
-    document.getElementById('login').classList.add("closedLogin")
-}
-
-function openConfirmLogin() {
-    closeLogin()
-    document.getElementById('confirmedLogin').classList.remove("closedLogin")
-    document.getElementById('confirmedLogin').classList.add("openedLogin")
-}
-
-function closeConfirmLogin() {
-    document.getElementById('confirmedLogin').classList.remove("openedLogin")
-    document.getElementById('confirmedLogin').classList.add("closedLogin")
-}
-
-function openReserve() {
-    document.getElementById('reserve').classList.remove("closedReserve")
-    document.getElementById('reserve').classList.add("openedReserve")
-}
-
-function closeReserve() {
-    document.getElementById('reserve').classList.remove('openedReserve')
-    document.getElementById('reserve').classList.add('closedReserve')
-}
-
-function openConfirmReserve() {
-    closeReserve()
-    document.getElementById('confirmedReserve').classList.remove("closedReserve")
-    document.getElementById('confirmedReserve').classList.add("openedReserve")
-}
-
-function closeConfirmReserve() {
-    document.getElementById('confirmedReserve').classList.remove("openedReserve")
-    document.getElementById('confirmedReserve').classList.add("closedReserve")
-}
-
-function startLogin() {
-    const buttonCloseLogin = document.getElementById('close-login')
-    const buttonOpenLogin = document.getElementById('open-login')
-    const buttonMakeLogin = document.getElementById('make-login')
-    const buttonCloseConfLogin = document.getElementById('close-ConfLogin')
-    const buttonLogout = document.getElementById('logout')
-
-    buttonCloseLogin.addEventListener("click", closeLogin)
-    buttonOpenLogin.addEventListener("click", openLogin)
-    buttonMakeLogin.addEventListener("click", makeLogin)
-    buttonCloseConfLogin.addEventListener("click", closeConfirmLogin)
-    buttonLogout.addEventListener("click", logout)
-}
-
-function startReserve() {
-    const buttonCloseReserve = document.getElementById('close-reserve')
-    const buttonOpenReserve = document.getElementById('open-reserve')
-    const buttonMakeReserve = document.getElementById('make-reserve')
-    const buttonCloseConfReserve = document.getElementById('close-ConfReserve')
-
-    buttonCloseReserve.addEventListener("click", closeReserve)
-    buttonOpenReserve.addEventListener("click", openReserve)
-    buttonMakeReserve.addEventListener("click", makeReserve)
-    buttonCloseConfReserve.addEventListener("click", closeConfirmReserve)
-}
-
-function makeLogin() {
-    let tuser = document.getElementById('txtuser').value
-    let tpass = document.getElementById('txtpass').value
-    let error = document.getElementById('errorLogin')
-
-    if (tuser == 0 || tpass == 0) {
-        error.innerHTML = `Informações incompletas!`
-    } else {
-        logado = true
-        openConfirmLogin()
-        login()
+let button = document.getElementById('read-button')
+button.addEventListener('click', function() {
+    let regras = document.querySelector('.regras')
+    regras.classList.toggle('active')
+    if (regras.classList.contains('active')) {
+        return button.textContent = "Ler Menos"
     }
-}
+    button.textContent = "Ler Mais +"
+})
 
-function makeReserve() {
-    let tdate = new Date(document.getElementById("txtdate").value).toLocaleDateString('pt-BR', { timeZone: 'UTC' })
-    let tsalao = document.getElementById("txtsalao").value
-    let tpessoas = Number(document.getElementById("txtpessoas").value)
-    let thora = document.getElementById("txthora").value
-    let error = document.getElementById('error')
-
-    if (tdate == 'Invalid Date' || tsalao == '' || tpessoas <= 0 || thora == 0) {
-        error.innerHTML = `Informações incompletas!`
-    } else if (logado == false) {
-        error.innerHTML = `É necessário fazer login primeiro!`
-    } else {
-        reservar()
-    }
-}
-
-function login() {
-    let tuser = document.getElementById('txtuser').value
-    let twelcome = document.getElementById('pwelcome')
-    twelcome.innerHTML = `Bem-vindo(a), ${tuser}`
-}
-
-function logout() {
-    logado = false
-    closeConfirmLogin()
-    openLogin()
-}
-
-function reservar() {
-    openConfirmReserve()
-    let num = getRandomNum(1000, 2000)
-    let letter = getRandomString(1)
-    let tdate = new Date(document.getElementById("txtdate").value).toLocaleDateString('pt-BR', { timeZone: 'UTC' })
-    let tsalao = document.getElementById("txtsalao").value
-    let tpessoas = Number(document.getElementById("txtpessoas").value)
-    let thora = document.getElementById("txthora").value
-    let tuser = document.getElementById('txtuser').value
-
-    let codigo = letter + num
-
-    let pdate = document.getElementById('pdate')
-    let psalao = document.getElementById('psalao')
-    let ppessoas = document.getElementById('ppessoas')
-    let phora = document.getElementById('phora')
-    let reservante = document.getElementById('reservante')
-
-    cod.innerHTML = `#${codigo}`
-    reservante.innerHTML = `<strong>Nome:</strong> ${tuser}`
-    pdate.innerHTML = `<strong>Dia:</strong> ${tdate}`
-    psalao.innerHTML = `<strong>Local:</strong> ${tsalao}`
-    ppessoas.innerHTML = `<strong>Quant. de Pessoas:</strong> ${tpessoas} pessoas`
-    phora.innerHTML = `<strong>Horário:</strong> ${thora}h`
-}
-
-function getRandomNum(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
-} // Essa função gera um número inteiro para o código
-
-function getRandomString(tamanho) {
-    var stringAleatoria = ''
-    var caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for (var i = 0; i < tamanho; i++) {
-        stringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length))
-    }
-    return stringAleatoria
-}
-
-startReserve()
-startLogin()
+produtos();
